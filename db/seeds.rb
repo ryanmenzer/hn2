ef16bbb9b783f7bce860b0d009b11f27d5360d0f
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-25 09:10:12 -0500

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,25 @@
+require 'faker'
+
 5.times do
+  User.create(email: Faker::Internet.email,
+              password: Faker::Internet.password,
+              name: Faker::Name.name,
+              about: Faker::Lorem.paragraph(sentence_count = 1, supplemental = false, random_sentences_to_add = 2))
+end
+
+User.all.each do |user|
+  3.times do
+    Post.create(title: Faker::Lorem.sentence, 
+                body: Faker::Lorem.paragraph(sentence_count = 3, supplemental = false, random_sentences_to_add = 7),
+                user_id: user.id)
+  end
+end
+
+Post.all.each do |post|
+  rand(1..5).times do
+    user_num = rand(1..(User.all.count))
+    Comment.create(body: Faker::Lorem.paragraph(sentence_count = 1, supplemental = false, random_sentences_to_add = 2),
+                  post_id: post.id,
+                  user_id: user_num)
+  end
+end
